d97bde4616d9fe0396a45685da50c49b9df03320
M0neyKick <khalid.ozal26@gmail.com>
2017-06-02 14:47:43 -0400

    Add files via upload

---
+++ final.java
@@ -0,0 +1,33 @@
+/* Main menu code
+ * 
+ * PLAY
+ * RULES (A method that's print's rules) >> Exit Rules >> BACK TO MAIN MENU
+ * QUIT (Create a method for exit) >> quits the game
+ * 
+ * */
+
+
+
+/* PLAY will start the game
+ * 
+ * First it will ask for name
+ * 
+ * Then ask for 2nd user name
+ * 
+ * then press enter to start the game
+ * 
+ * */
+
+/* Rules method will print just print rules
+ * 
+ * It will have an option to exit the rule method
+ * 
+ * */
+
+/*Quit 
+ * 
+ * Method for quit program 
+ * 
+ * Method to exit rules method
+ * */
+
