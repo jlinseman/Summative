1878bfc488ef84fd74d2f6f20569f85d7de54807
火兄弟 <khalid.ozal26@gmail.com>
2017-06-07 18:02:20 -0400

    Update trivialPursuit.java

--- trivialPursuit.java
+++ trivialPursuit.java
@@ -10,14 +10,26 @@ class trivialPursuit
 }
   
 // Print rules method
-  public static void rulesPrint()
+  public static void rulesPrint(String getMenu, String goBack)
   {
-    
+    if(getMenu.equals  ("Play") || getMenu.equals  ("play"))
+      System.out.println("To go back to Main menu please enter Main");
+      System.out.println("This game is to be played with two players");
+      System.out.println();
+      System.out.println("Their are points given to player depending on the hardness of the question");
+  }
+  {
+      goBack = In.getString();
+      if (goBack.equals ("Main") || goBack.equals ("main"))
+       
+      
   }
   
 // Main method    
   public static void main (String[] args)
   {
+    String goBack = "";
+    String getMenu = "";
     // Main menu
     System.out.println("Welcome to Trivial Pursuit");
     System.out.println("");  
@@ -25,8 +37,9 @@ class trivialPursuit
     System.out.println("RULES(To see rules enter Rules)");
     System.out.println("QUIT(To quit enter 0)");
     System.out.println("");
-    String getMenu = "";
+
     getMenu = In.getString();
     proQuit(getMenu);
+    rulesPrint(getMenu);
   }
 }
